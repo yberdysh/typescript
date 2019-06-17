2d193f5c0d5859b572b7d27e8213476f13f9a3cd
Jane Costa <yberdysh@gmail.com>
2019-06-16 23:09:38 -0400

    typescript practice

--- greeter.js
+++ greeter.js
@@ -10,6 +10,22 @@ var Student = /** @class */ (function () {
 function greeter(person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
+function tester(numbers) {
+    var final = 0;
+    for (var i = 0; i < numbers.length; i++) {
+        final += numbers[i];
+    }
+    return final;
+}
+var zhoe = { firstName: "Joe", lastName: "Costa", age: "27", properties: { eyes: "brown", hair: "black" } };
+function greet(person) {
+    return "Name: " + person.firstName + " " + person.lastName + ", age: " + person.age + ", eyes: " + person.properties.eyes + ", hair: " + person.properties.hair;
+}
 // let user = {firstName: "Jane", lastName: "Costa"}
 var user = new Student("Joe", "D", "Costa");
-document.body.innerHTML = greeter(user);
+var isDone = false;
+var lines = 42;
+var someName = "Anders";
+console.log(isDone);
+document.body.innerHTML = greet(zhoe);
+// document.body.innerHTML = tester([1,2,3,4,5])
--- greeter.ts
+++ greeter.ts
@@ -15,7 +15,40 @@ function greeter(person: Person){
   return "Hello, " + person.firstName + " " + person.lastName;
 }
 
+function tester(numbers: Array<number>){
+  let final = 0
+  for (let i = 0; i < numbers.length; i++){
+    final += numbers[i]
+  }
+  return final
+}
+
+interface nestedPerson {
+  firstName: string;
+  lastName: string;
+  // optional type with ?
+  age?: any;
+  properties: {
+    eyes: string;
+    hair: string
+  }
+}
+
+let zhoe = {firstName: "Joe", lastName: "Costa", age: "27", properties: {eyes: "brown", hair: "black"}}
+
+function greet(person: nestedPerson){
+  return `Name: ${person.firstName} ${person.lastName}, age: ${person.age}, eyes: ${person.properties.eyes}, hair: ${person.properties.hair}`
+}
+
 // let user = {firstName: "Jane", lastName: "Costa"}
 let user = new Student("Joe", "D", "Costa")
 
-document.body.innerHTML = greeter(user)
+
+let isDone: boolean = false;
+let lines: number = 42;
+let someName: string = "Anders";
+
+console.log(isDone)
+
+document.body.innerHTML = greet(zhoe)
+// document.body.innerHTML = tester([1,2,3,4,5])
